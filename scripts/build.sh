4173cc9c1d036b92c48e814d547837a481ed5df7
niedzwiedzw <niedzwiedzwo@gmail.com>
2023-01-10 09:51:32 +0100

    ask before rebuilding

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# shamelessly stolen from https://stackoverflow.com/a/29436423/9014011
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
 echo zig build shell started
 ZIG_BOOTSTRAP_PATH="/zig-bootstrap"
 ZIG_PATH="/zig"
@@ -9,8 +20,18 @@ MCPU="baseline"
 
 echo "preparing zig using zig bootstrap"
 cd "${ZIG_BOOTSTRAP_PATH}"
-./build "${TARGET}" "${MCPU}"
+yes_or_no "rebuild bootstrap? (takes ~700 years)" && ./build "${TARGET}" "${MCPU}"
 cd -
 echo "[DONE] preparing zig using zig bootstrap"
 
+ZIG_PREFIX="${ZIG_BOOTSTRAP_PATH}/out/zig-${TARGET}-${MCPU}"
+LLVM_PREFIX="${ZIG_BOOTSTRAP_PATH}/out/${TARGET}-${MCPU}"
+ZIG_BIN="${ZIG_PREFIX}/bin/zig"
+
+cd "${ZIG_PATH}";
+
+echo "alias rebuild-zig=\"${ZIG_BIN} build -p stage3 --search-prefix ${LLVM_PREFIX} --zig-lib-dir lib -Dstatic-llvm\"" >> ~/.bashrc
+
+
+echo "[${ZIG_PATH}] all set up, type 'rebuild-zig' to rebuild"
 bash
